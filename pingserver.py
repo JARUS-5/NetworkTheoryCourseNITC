477f2abd67f4903fcb41fe7198bc6680bb283a5b
SURAJ S <gssacharya@gmail.com>
2020-01-14 18:01:36 +0530

    default server facebook added, ttl added

--- pingserver.py
+++ pingserver.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Author: Suraj S (B170389EC)
 Contributors: Sayed Muhsin & Senna Manoj
@@ -6,7 +5,7 @@ Contributors: Sayed Muhsin & Senna Manoj
 Python Version: 3.6.9
 OS: Ubuntu 18.04
 
-ICMP echo structure and variables source: https://tools.ietf.org/html/rfc792#page-14
+ICMP echo source: https://tools.ietf.org/html/rfc792#page-14
 """
 
 import socket
@@ -14,16 +13,21 @@ import sys
 import time
 
 try:
-    s = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.getprotobyname("icmp"))
+    s = socket.socket(socket.AF_INET,
+                      socket.SOCK_RAW,
+                      socket.getprotobyname("icmp"))
 except:
-    print("Program requires root permission or Error with assigning socket.")
-    print("Try typing this in terminal: 'sudo python3 pingserver.py'")
+    print("Program requires root permission. Error with assigning socket.")
     sys.exit()
 
-addr = input("Enter server address: ")
+addr = input("Enter server address (leave blank for 'facebook.com'): ")
 ct = int(input("Enter number of times to ping ( -1 to send infinitely ): "))
 
-temp = addr.split('.')
+if addr=='':
+    addr = 'facebook.com'
+    temp = ['facebook','com']
+else:
+    temp = addr.split('.')
 
 try:
     temp_var = int(temp[-1])
@@ -78,12 +82,17 @@ def send_packet(skt,hd,ip,data):
     rec = skt.recv(2048)
     end_time = time.time()
     tot_time = (end_time-start_time)*1000
-    #print(rec)
-    print("{} bytes of data from {} :".format(len(data),ip),end=' ')
-    print("icmp_seq= {} time={}ms".format(hd[7],tot_time))
+    rec = list(rec)
+    ttl = rec[8]
+    data_length = len(data)
+    if data_length%2==1:
+        data_length = data_length+1   # 1 bytes of zeros are padded
+    d_size = 8 + data_length
+    print("{} bytes of data from {} :".format(d_size,ip),end=' ')
+    print("icmp_seq= {0} ttl={1} time={2:0.3f}ms".format(hd[7],ttl,tot_time))
 
 try:
-    s.connect(('%s'%addr,80))
+    s.connect(('%s'%addr,8888))
 except:
     print("Cannot connect to server.")
     sys.exit()
@@ -102,7 +111,7 @@ if ct==-1:
             header[7] = seq
             
 else:
-    for i in range(1,ct+1,1):
+    for i in range(1,abs(ct)+1,1):
         seq = i
         if seq>65535:  #Reset sequence if it is greater than 0xffff
             seq = 1
